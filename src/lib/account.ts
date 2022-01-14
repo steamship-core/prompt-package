ed7978612e937d9f256c8c8fcad948114bd022c5
Edward Benson <edward.benson@gmail.com>
2022-01-14 16:35:34 -0500

    ain

---
+++ src/lib/account.ts
@@ -0,0 +1,41 @@
+
+import { ApiBase, Response } from './api_base';
+import { Configuration } from './shared/Configuration';
+
+const _EXPECT = (client: ApiBase, data: unknown) => { 
+  return new User(client, data as UserParams) 
+}
+
+export interface UserParams {
+  id?: string;
+  name?: string;
+  handle?: string;
+}
+
+export class User {
+  id?: string;
+  name?: string;
+  handle?: string;
+  client: ApiBase;
+
+  constructor(client: ApiBase, params: UserParams) {
+    this.client = client;
+    this.id = params.id;
+    this.name = params.name;
+    this.handle = params.handle;
+  }
+
+  static async current(
+    client: ApiBase,
+    config?: Configuration
+  ): Promise<Response<User>> {
+    return (await client.post(
+      'account/current',
+      {},
+      {
+        expect: _EXPECT,
+        ...config
+      },
+    )) as Response<User>;
+  }
+}
