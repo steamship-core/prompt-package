db61ce11a9da8ceeed440c25bb4152e0e9f2856e
Ted Benson <ted@steamship.com>
2022-12-22 15:56:56 -0500

    Add exception thrower

--- src/lib/api_base.ts
+++ src/lib/api_base.ts
@@ -18,13 +18,13 @@ import {
 import { isNode } from './utils';
 
 axios.interceptors.response.use(
-  response => response,
-  error => {
+  (response) => response,
+  (error) => {
     // We really want to throw the error so it is handled and we don't get
     // an unhandledrejection error. By throwing here, we are handling the
     // rejection, and bubbling up to the closest error handler (try/catch or
     // catch method call on a promise).
-    throw error
+    throw error;
   }
 );
 
