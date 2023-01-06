eb6013ad1c31a6878cf93b4fc43fad57ab047b45
Ted Benson <ted@steamship.com>
2023-01-06 14:38:18 -0500

    Fix export statement

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ export { default as regeneratorRuntime } from 'regenerator-runtime';
 export { File } from './lib/file.js';
 export {
   LogEntry,
+} from './lib/log.js';
+export {
   LogEntryList,
   LogEntryListParams,
   LogEntryParams,
